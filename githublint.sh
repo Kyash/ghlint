0fdcfd3e48a3ca4466c8b568beff312c54aa2f3b
Naoki Kimura <n.kimura.cap@gmail.com>
2021-03-10 17:56:20 +0900

    Fix a bug

--- githublint.sh
+++ githublint.sh
@@ -515,7 +515,7 @@ function to_tsv() {
     codeowners
     protected_branches
   )
-  jq -r --args '$ARGS.positional + (.rules | map(.signature)) | @tsv' "${fields[*]}" < "$rules_dump"
+  jq -r --args '$ARGS.positional + (.rules | map(.signature)) | @tsv' "${fields[@]}" < "$rules_dump"
 
   jq --seq -r --arg org "$org" --argfile rules "$rules_dump" '
     (
