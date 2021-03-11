da4c314f6b5e22c7599732d564fca8a400cbdb42
Naoki Kimura <n.kimura.cap@gmail.com>
2021-03-11 13:13:31 +0900

    Update json_seq.sh

--- lib/json_seq.sh
+++ lib/json_seq.sh
@@ -2,6 +2,10 @@ function json_seq::new() {
   printf '\x1e'
   if [ $# -ne 0 ]
   then
+    if [ $XTRACE -ne 0 ]
+    then
+      echo "$@" | xargs -L 1 jq -c '.'
+    fi >&2
     jq -sc 'add' "$@"
   else
     jq -c
