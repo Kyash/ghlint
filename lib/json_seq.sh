d7ddbf76b38300273d99c120642b4233ae3de52f
Naoki Kimura <n.kimura.cap@gmail.com>
2021-03-11 13:29:50 +0900

    Update json_seq.sh

--- lib/json_seq.sh
+++ lib/json_seq.sh
@@ -2,11 +2,13 @@ function json_seq::new() {
   printf '\x1e'
   if [ $# -ne 0 ]
   then
+    local filter='add'
     if [ $XTRACE -ne 0 ]
     then
       echo "$@" | xargs -L 1 jq -c '.'
+      filter='def log(logger; f): . as $o | (f | logger) | $o; log(debug; {length:(length),types:(map(type))}) | add'
     fi >&2
-    jq -sc 'add' "$@"
+    jq -sc "$filter" "$@"
   else
     jq -c
   fi
