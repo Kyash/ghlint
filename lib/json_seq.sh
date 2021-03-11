38e29b1105ced48cece8c97caa89119da8686b52
Naoki Kimura <n.kimura.cap@gmail.com>
2021-03-11 14:04:29 +0900

    Delete trace codes

--- lib/json_seq.sh
+++ lib/json_seq.sh
@@ -3,11 +3,6 @@ function json_seq::new() {
   if [ $# -ne 0 ]
   then
     local filter='add'
-    if [ $XTRACE -ne 0 ]
-    then
-      echo "$@" | xargs -L 1 jq -c '.'
-      filter='def log(logger;f):. as $o|(f|logger)|$o;log(debug;{length:(length),types:(map(type)),test:(add)})|'$filter
-    fi >&2
     jq -sc "$filter" "$@"
   else
     jq -c
