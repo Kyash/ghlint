52ecd543ffe192e8a5609942cb444c244cb1654d
Naoki Kimura <n.kimura.cap@gmail.com>
2021-03-21 17:14:02 +0900

    Update repos.sh

--- repos.sh
+++ repos.sh
@@ -9,6 +9,7 @@ declare -r BUILD_OPTS=(--target stage-prd --file "$DOCKERFILE")
 declare -r CONTAINER_NAME="githublint_$$"
 
 function finally () {
+  command -v docker /dev/null || return
   docker ps -qf "name=$CONTAINER_NAME" | while IFS= read -r container_id
   do
     docker stop "$container_id"
@@ -17,6 +18,23 @@ function finally () {
 
 function main() {
   trap finally EXIT
+
+  local default_rc_file="$PWD/.githublintrc.json"
+  local rc_file=''
+  ! [ -f "$default_rc_file" ] || rc_file="$default_rc_file"
+
+  local args=()
+  while getopts ":c:de:f:hp:r:x" OPT
+  do
+    case "$OPT" in
+      c) rc_file="$OPTARG" ;;
+      e | f | p | r) args+=("-$OPT" "$OPTARG") ;;
+      d | x | h) args+=("-$OPT") ;;
+      ?) args+=("-$OPTARG") ;;
+    esac
+  done
+  shift $((OPTIND - 1))
+  args+=("$@")
 
   local image="docker.pkg.github.com/kyash/githublint/githublint:${VERSION}"
   if [ -n "$IMANGE" ]
@@ -27,11 +45,14 @@ function main() {
     image="$(docker build . -q "${BUILD_OPTS[@]}")"
   fi
 
+  docker_run_opts=()
+  [ "${rc_file:0:1}" = '/' ] || rc_file="$PWD/$rc_file"
+  [ -z "$rc_file" ] || docker_run_opts+=(-v "$rc_file:/home/curl_user/.githublintrc.json")
+
   docker run --name "$CONTAINER_NAME" --rm -e GITHUB_TOKEN \
-    -v "$PWD/.githublintrc.json:/home/curl_user/.githublintrc.json" \
     --mount "type=volume,src=githublint,dst=/home/curl_user/.githublint" \
-    "$image" "$@" |
-    grep '^\(kind\|repository\)\t' | cut -f2-
+    "${docker_run_opts[@]+"${docker_run_opts[@]}"}" \
+    "$image" "${args[@]+"${args[@]}"}"
 }
 
 main "$@"
